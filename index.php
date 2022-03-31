9e540d3f334ea7f2f52646f1cb7c455fcf61e7cb
zuzubaidillah <zuzubaidillah@gmail.com>
2022-03-31 09:23:18 +0700

    add library sweatalert

--- index.php
+++ index.php
@@ -114,6 +114,14 @@
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script>
+        Swal.fire({
+            icon: 'error',
+            title: 'Gagal',
+            text: 'Data Masih Kosong!'
+        })
+    </script>
 </body>
 
 </html>
